38ccb59fb46cd9716bdef9eb64ba0317765d0339
Chris Norish <norishchris@gmail.com>
2021-04-21 15:01:58 +0100

    feat: npm init + .env created

--- server/index.js
+++ server/index.js
@@ -6,8 +6,9 @@ const express = require('express')
 const router = require('./router');
 const cors = require('cors');
 const app = express();
+require('dotenv').config();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 
 app.use(cors());
