989b61a3aff4d2e9d37b3e5181a40ff90bdd8061
Chris Norish <norishchris@gmail.com>
2021-04-22 16:40:05 +0100

    feat: search/ register boxes drop down and track input

--- react/src/components/welcomechoicecomponent.jsx
+++ react/src/components/welcomechoicecomponent.jsx
@@ -1,25 +1,78 @@
 import { useState } from 'react';
 
-const WelcomeChoiceButton = ({searchType}) => {
+const WelcomeChoiceButton = ({ searchType }) => {
+  const [searchClicked, setSearchClicked] = useState(false);
+  const [logClicked, setLogClicked] = useState(false);
+  const [inputText, setInputText] = useState('');
 
 
-  const [searchClicked, setSearchClicked] = useState(false)
-  const [logClicked, setLogClicked] = useState(false)
-
+  const inputHandler = (event) => setInputText(event.target.value)
 
   return (
     <div>
-       <button
-         type="button"
-         onClick={() => {
-          searchType === 'Search Faults' ? setSearchClicked(!searchClicked) : setLogClicked(!logClicked);
-          }}
-         >{searchType}</button>
+      <button
+        type="button"
+        onClick={() => {
+          searchType === 'Search Faults'
+            ? setSearchClicked(!searchClicked)
+            : setLogClicked(!logClicked);
+        }}
+      >
+        {searchType}
+      </button>
+
+      {searchClicked && (
+        <div>
+          <form>
+            <label>
+              <input
+                type="text"
+                value={inputText}
+                placeholder="Enter a registration number..."
+                onChange={(event) => inputHandler(event)}
+              />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+
+          <p>Your reg:</p>
+          <p>{inputText.toString()}</p>
+        </div>
+      )}
+
+      {logClicked && (
+        <div>
+          <form>
+            <label>
+              <input
+                type="text"
+                value={inputText}
+                placeholder="Enter a registration number..."
+                onChange={(event) => inputHandler(event)}
+              />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+
+          <p>Your reg:</p>
+          <p>{inputText.toString()}</p>
+        </div>
+      )}
     </div>
-  )
+  );
+};
+
+export default WelcomeChoiceButton;
+
+{
+  /*<Text>Previous searches:</Text>
+      {previousSearches.map((element) => (
+        <Text key={element.id}>{element.reg}</Text>
+        ))}*/
 }
 
-export default WelcomeChoiceButton
-
-
-
+// <form onSubmit={this.handleSubmit}>        <label>
+//     Name:
+//     <input type="text" value={this.state.value} onChange={this.handleChange} />        </label>
+//   <input type="submit" value="Submit" />
+// </form>
