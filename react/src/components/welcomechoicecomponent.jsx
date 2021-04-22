f4f2cf119dfd6dafa1cb76f13adeaa0fd74d1028
Chris Norish <norishchris@gmail.com>
2021-04-22 17:19:01 +0100

    feat: extra forms added on welcome page, layout centered

--- react/src/components/welcomechoicecomponent.jsx
+++ react/src/components/welcomechoicecomponent.jsx
@@ -1,15 +1,22 @@
 import { useState } from 'react';
+import '../css/welcomecomponent.css'
 
 const WelcomeChoiceButton = ({ searchType }) => {
   const [searchClicked, setSearchClicked] = useState(false);
   const [logClicked, setLogClicked] = useState(false);
-  const [inputText, setInputText] = useState('');
+
+  const [inputText, setInputText] = useState({
+    reg: '',
+    make: '',
+    model: ''
+  });
 
 
-  const inputHandler = (event) => setInputText(event.target.value)
+  const inputHandler = (event) => setInputText({...inputText, [event.target.name]: event.target.value,})
+
 
   return (
-    <div>
+    <div className='button-div'>
       <button
         type="button"
         onClick={() => {
@@ -21,41 +28,76 @@ const WelcomeChoiceButton = ({ searchType }) => {
         {searchType}
       </button>
 
+      {/*IF SEARCH FAULTS BUTTON IS CLICKED*/}
       {searchClicked && (
-        <div>
+        <div className='input-box-div'>
           <form>
             <label>
               <input
                 type="text"
-                value={inputText}
+                name='reg'
+                value={inputText.reg}
                 placeholder="Enter a registration number..."
                 onChange={(event) => inputHandler(event)}
               />
             </label>
-            <input type="submit" value="Submit" />
-          </form>
 
           <p>Your reg:</p>
-          <p>{inputText.toString()}</p>
+          <p>{inputText.reg}</p>
+            <input type="submit" value="Submit" />
+          </form>
         </div>
       )}
 
+      {/*IF REGISTER FAULT BUTTON IS CLICKED*/}
       {logClicked && (
-        <div>
+        <div className='input-box-div'>
           <form>
             <label>
               <input
                 type="text"
-                value={inputText}
-                placeholder="Enter a registration number..."
+                name='reg'
+                value={inputText.reg}
+                placeholder="Enter your registration number..."
                 onChange={(event) => inputHandler(event)}
               />
             </label>
-            <input type="submit" value="Submit" />
-          </form>
 
           <p>Your reg:</p>
-          <p>{inputText.toString()}</p>
+          <p>{inputText.reg}</p>
+
+
+
+          <label>
+              <input
+                type="text"
+                name='make'
+                value={inputText.make}
+                placeholder="Enter your vehicles manufacturer..."
+                onChange={(event) => inputHandler(event)}
+              />
+            </label>
+
+          <p>Make:</p>
+          <p>{inputText.make}</p>
+
+
+          <label>
+              <input
+                type="text"
+                name='model'
+                value={inputText.model}
+                placeholder="Enter the model..."
+                onChange={(event) => inputHandler(event)}
+              />
+            </label>
+
+          <p>Make:</p>
+          <p>{inputText.model}</p>
+
+
+            <input type="submit" value="Submit" />
+          </form>
         </div>
       )}
     </div>
