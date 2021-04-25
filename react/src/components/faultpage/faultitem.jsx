129a9a29c7c4a51950b14ad2b60d28e4fde94127
Chris Norish <norishchris@gmail.com>
2021-04-25 16:47:58 +0100

    feat: added images

--- react/src/components/faultpage/faultitem.jsx
+++ react/src/components/faultpage/faultitem.jsx
@@ -1,13 +1,42 @@
 import '../../css/faultpage.css';
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 
-const FaultItem = ({ summary, description, year, area, priceToFix, faultLogged, rating }) => {
+import ImageViewer from 'react-simple-image-viewer';
 
-  const [expand, setExpand] = useState(false)
+const FaultItem = ({
+  summary,
+  description,
+  year,
+  area,
+  priceToFix,
+  faultLogged,
+  rating,
+}) => {
+  const [currentImage, setCurrentImage] = useState(0);
+  const [isViewerOpen, setIsViewerOpen] = useState(false);
+
+  const [expand, setExpand] = useState(false);
 
   // const handleRatingChange = (upOrDown) => {
   //   console.log(upOrDown)
   // }
+
+  const images = [
+    'http://placeimg.com/1200/800/nature',
+    'http://placeimg.com/800/1200/nature',
+    'http://placeimg.com/1920/1080/nature',
+    'http://placeimg.com/1500/500/nature',
+  ];
+
+  const openImageViewer = useCallback((index) => {
+    setCurrentImage(index);
+    setIsViewerOpen(true);
+  }, []);
+
+  const closeImageViewer = () => {
+    setCurrentImage(0);
+    setIsViewerOpen(false);
+  };
 
   return (
     <div className="entire-container">
@@ -18,22 +47,46 @@ const FaultItem = ({ summary, description, year, area, priceToFix, faultLogged, 
       <div className="main-fault-section">
         <p>Summary: {summary}</p>
         <p>Description: {description}</p>
-        {expand &&
-          <div className='expanded-text'>
-            <p>Area of fault: {area}</p>
-            <p>Build year: {year}</p>
-            <p>Price to fix: £{priceToFix}</p>
-            <p>Reported on: {faultLogged}</p>
+        {expand && (
+          <div className="expanded-container">
+            <div className="expanded-text">
+              <p>Area of fault: {area}</p>
+              <p>Build year: {year}</p>
+              <p>Price to fix: £{priceToFix}</p>
+              <p>Reported on: {faultLogged}</p>
+            </div>
+
+            <div className="image-section">
+              {images.map((src, index) => (
+                <img
+                  src={src}
+                  onClick={() => openImageViewer(index)}
+                  width="300"
+                  key={index}
+                  style={{ margin: '2px' }}
+                  alt=""
+                />
+              ))}
+
+              {isViewerOpen && (
+                <ImageViewer
+                  src={images}
+                  currentIndex={currentImage}
+                  onClose={closeImageViewer}
+                />
+              )}
+            </div>
           </div>
-        }
-      </div>
-      <div className="right-fault-section">
-        <button type="button" onClick={(() => setExpand(!expand))}>Expand</button>
+        )}
       </div>
 
+      <div className="right-fault-section">
+        <button type="button" onClick={() => setExpand(!expand)}>
+          Expand
+        </button>
+      </div>
     </div>
   );
 };
 
 export default FaultItem;
-
--- react/src/components/faultpage/rightdatadisplay.jsx
+++ react/src/components/faultpage/rightdatadisplay.jsx
@@ -1,60 +1,48 @@
-import { Pie } from 'react-chartjs-2';
+import { Doughnut } from 'react-chartjs-2';
 import '../../css/faultpage.css';
 
-const RightDataDisplay = ({allFaultsObject}) => {
-
+const RightDataDisplay = ({ allFaultsObject }) => {
   const data = {
-  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-  datasets: [
-    {
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)',
-      ],
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)',
-      ],
-      borderWidth: 1,
-    },
-  ],
-};
-
-
-
-
+    labels: ['Red', 'Blue', 'Yellow', 'Green'],
+    datasets: [
+      {
+        label: 'Problem Areas',
+        data: [12, 19, 3, 5],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
 
   return (
-    <div className='data-container'>
-  {allFaultsObject.length > 0 &&
-    <div>
-      <Pie data={data} />
-      <div>
-        <p>Largest problem area: {allFaultsObject[0].faults[0].area}</p>
-        <p>EXTRA STATS</p>
-        <p>EXTRA STATS</p>
-      </div>
-      </div>
-  }
+    <div className="data-container">
+      {allFaultsObject.length > 0 && (
+        <div>
+          <Doughnut
+            data={data}
+            width={400}
+            height={400}
+          />
+          <div>
+            <p>Largest problem area: {allFaultsObject[0].faults[0].area}</p>
+            <p>EXTRA STATS</p>
+            <p>EXTRA STATS</p>
+          </div>
+        </div>
+      )}
     </div>
+  );
+};
 
-
-  )
-}
-
-export default RightDataDisplay
-
-
-
-
-
+export default RightDataDisplay;
