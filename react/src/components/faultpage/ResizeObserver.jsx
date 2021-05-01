db55751809a61db9352dd2f4ddf871efcc1fa020
ignaraph <ignacioraphael@hotmail.com>
2021-05-01 14:44:08 +0200

    Test: Right side bar tests completed

---
+++ react/src/components/faultpage/ResizeObserver.jsx
@@ -0,0 +1,11 @@
+class ResizeObserver {
+  observe() {
+    //nothing
+  }
+  unobserve() {
+    //do nothing
+  }
+}
+
+window.ResizeObserver = ResizeObserver
+export default ResizeObserver;
---
+++ react/src/components/faultpage/rightDataDisplay.test.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import RightDataDisplay from './rightdatadisplay';
+import '@testing-library/jest-dom/extend-expect';
+import userEvent from '@testing-library/user-event';
+import { fireEvent, render, screen } from '@testing-library/react';
+import ResizeObserver from './ResizeObserver';
+
+
+const allFaultsObject= [{faults:['test1','test2','test3']}]
+beforeEach(() => {
+  render(<RightDataDisplay  allFaultsObject={allFaultsObject} />)
+})
+
+describe('component should render',() => {
+  test('mainContainer', () => {
+    expect(screen.getByTestId('rightDataDisplayContainer')).toBeInTheDocument();
+  })
+
+  test('problem areas', () => {
+    expect(screen.getByText('Problem areas')).toBeInTheDocument();
+  })
+
+  test('Average repair price', () => {
+    expect(screen.getByText('Average repair price')).toBeInTheDocument();
+  })
+
+  test('Number of faults by year', () => {
+    expect(screen.getByText('Number of faults by year')).toBeInTheDocument();
+  })
+})
+
+describe('data should change', () => {
+
+  beforeEach(() => {
+  jest.clearAllMocks();
+  })
+
+  test('Problem area data should show', () => {
+    const problemAreasBtn = (screen.getByText('Problem areas'))
+    userEvent.click(problemAreasBtn)
+    expect(screen.getByText('Problem area data')).toBeInTheDocument();
+  })
+
+  test('Average Repair Price Data should show', () => {
+    const averageRepairPriceBtn = (screen.getByText('Average repair price'))
+    userEvent.click(averageRepairPriceBtn)
+    expect(screen.getByText('Average Repair Price data')).toBeInTheDocument();
+  })
+
+  test('Number of faults by year should show', () => {
+    const numOfFaultsBtn = (screen.getByText('Number of faults by year'))
+    userEvent.click(numOfFaultsBtn)
+    expect(screen.getByText('Faults Sorted By Year data')).toBeInTheDocument();
+  })
+
+})
